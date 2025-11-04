a978f256212bbdc64766565dd9f22325f5a3620e
Svetlana Zlt <svetlalanaz@gmail.com>
2025-11-04 21:12:06 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
+"use strict";
+function solveEquation (a,b,c) {
+let discr = (b**2)-4*a*c;
+
+if (discr < 0) {
+  arr = [0];
+  return arr;
+}
+if (discr == 0) {
+  arr = [-b/(2*a)];
+  return arr;
+
+}
+if (discr > 0) {
+  arr =  [(-b + Math.sqrt(d))/(2*a)', '(-b - Math.sqrt(d) )/(2*a)] ;
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+
 }
