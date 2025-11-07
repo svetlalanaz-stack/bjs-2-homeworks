5796c820085a8d01dcdd02cac265bd831bf1ff43
Svetlana Zlt <svetlalanaz@gmail.com>
2025-11-07 20:23:04 +0300

    Changes 7.11

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,71 +1,66 @@
 function getArrayParams (...array) {
   let min = Math.min(...array);
   let max = Math.max(...array);
-  let sum =  [1, 2, 3, -100, 10].reduce(function (a, b) {
+  let sum =  [...array].reduce(function (a, b) {
     return a+b;
     });
   
   let avg = +(sum/array.length).toFixed(2);
   
-    console.log(min);
-    console.log(max);
-    console.log(sum);
-    console.log(avg);
-  console.log("min: " + min + ", " + "max: " + max + ", " + "avg: " + avg )
+   return "min: " + min + ", " + "max: " + max + ", " + "avg: " + avg;
   }
   
   getArrayParams (1, 2, 3, -100, 10);
 
 
 
-function summElementsWorker (...array1) {
-    let sum = [10, 10, 11, 20, 10].reduce(function (a, b) {
+function summElementsWorker (...array) {
+    let sum = [...array].reduce(function (a, b) {
       return a+b;
       });
-      console.log(sum); 
+      return sum; 
 
   
 }
 summElementsWorker (10, 10, 11, 20, 10);
 
 
-function differenceMaxMinWorker (...array2) {
+function differenceMaxMinWorker (...array) {
 
-  let min = Math.min(...array2);
-  let max = Math.max(...array2);
+  let min = Math.min(...array);
+  let max = Math.max(...array);
   
-  console.log(max-min);
+  return (max-min);
 
 }
 
 differenceMaxMinWorker (10, 10, 11, 20, 10);
 
 
-function differenceEvenOddWorker (...array3) {
+function differenceEvenOddWorker (...array) {
   let sumEvenElement = 0;
   let sumOddElement = 0;
 
-  let arrayDifferenceEvenOdd = [10, 10, 11, 20, 10];
+  let arrayDifferenceEvenOdd = [...array];
   for (let i = 0; i < arrayDifferenceEvenOdd.length; i++) {
     if (arrayDifferenceEvenOdd[i] % 2 == 0) {
       sumEvenElement += arrayDifferenceEvenOdd[i];
-    }
-    else {
+    } else {
       sumOddElement += arrayDifferenceEvenOdd[i];
     }
   }
 
-console.log(sumEvenElement-sumOddElement);
+return sumEvenElement-sumOddElement;
 }
 
 differenceEvenOddWorker (10, 10, 11, 20, 10);
 
 
-function averageEvenElementsWorker (...array4) {
+function averageEvenElementsWorker (...array) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
 
-  let arrayAverageEvenElements = [10, 10, 11, 20, 10];
+  let arrayAverageEvenElements = [...array];
   for (let i = 0; i < arrayAverageEvenElements.length; i++) {
     if (arrayAverageEvenElements[i] % 2 == 0) {
       sumEvenElement += arrayAverageEvenElements[i];
@@ -74,11 +69,31 @@ function averageEvenElementsWorker (...array4) {
    
    }
 
- console.log(sumEvenElement/countEvenElement);
+ return sumEvenElement/countEvenElement;
  }
  
  averageEvenElementsWorker (10, 10, 11, 20, 10);
 
 
+
+
+ function makeWork (...arrOfArr, afunc) {
+  let arrOfArr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]]; 
+  let maxWorkerResult = -Infinity;
+  
+  for (let i = 0; i < arrOfArr.length; i++) {
+   
+    if (arrOfArr[i] > maxWorkerResult) {
+      maxWorkerResult = arrOfArr[i];
+    }
+    }
+  
+   return  maxWorkerResult;
+   
+  }
+  
+  
+  
+  makeWork (arrOfArr, summElementsWorker);
 
 
