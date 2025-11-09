f4b89b2615e7acf03b55584d537fea92c1a4a577
Svetlana Zlt <svetlalanaz@gmail.com>
2025-11-09 22:14:12 +0300

    Changes 09.11

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,28 @@
 "use strict";
 function solveEquation (a,b,c) {
-let discr = (b**2)-4*a*c;
+  let discr = (b**2)-4*a*c;
 
-if (discr < 0) {
-  arr = [0];
-  return arr;
-}
-if (discr == 0) {
-  arr = [-b/(2*a)];
-  return arr;
+    if (discr == 0) {
+      arr = [-b/(2*a)];
+  }
 
-}
-if (discr > 0) {
-  arr =  [(-b + Math.sqrt(d))/(2*a)', '(-b - Math.sqrt(d) )/(2*a)] ;
-  return arr;
+    if (discr > 0) {
+      arr =  [(-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d) )/(2*a)] ;
+    }
+
+return arr;
+
 }
 
 
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let percent = [0 - 1];
+  let countMonths = 1;
+  let contribution;
+  let amount;
+  let debtBody = amount - contribution;
+  let monthPay = debtBody * (percent + (percent / (((1 + percent)**n) - 1)));
+  let sum = +(monthPay + countMonths).toFixed(2);
 
+  return sum;
 }
